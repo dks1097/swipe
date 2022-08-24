1a12ad1144b0e493e0fe107f9e939efde4fade2a
dks1097 <carlos.d.t.m19@gmail.com>
2022-08-24 20:05:34 +0100

    Set Animated Cards

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Component } from "react";
+import {  Card, Button,Image } from "react-native-elements";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Ball from "./src/ball";
+import Deck from "./src/Deck";
+
+const DATA = [
+  {
+    id: 1,
+    text: "Card #1",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg",
+  },
+  {
+    id: 2,
+    text: "Card #2",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg",
+  },
+  {
+    id: 3,
+    text: "Card #3",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+  },
+  {
+    id: 4,
+    text: "Card #4",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg",
+  },
+  {
+    id: 5,
+    text: "Card #5",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg",
+  },
+  {
+    id: 6,
+    text: "Card #6",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg",
+  },
+  {
+    id: 7,
+    text: "Card #7",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+  },
+  {
+    id: 8,
+    text: "Card #8",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg",
+  },
+];
+
+class App extends Component {
+  renderCard(item) {
+    return (
+    <Card 
+      key={item.id}
+      title={item.text}
+    >
+      <Image source={{uri: item.uri}} style={{height: 200, width: 200}}/>
+      <Text style={{marginBottom: 10}}>
+        {item.text}
+        </Text>
+      <Button
+        icon={{ name: "code" }}
+        backgroundColor="#03A9F4"
+        title="View Now"
+      />
+    </Card>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Deck data={DATA} renderCard={this.renderCard} />
+      </View>
+    );
+  }
 }
 
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
